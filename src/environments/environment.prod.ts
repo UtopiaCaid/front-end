cc6e7c509fe121267b664a4226d8d97a3ddf3e87
IanSmoothStack <ian.agostini@smoothstack.com>
2021-04-21 14:39:39 -0400

    fixed bug I think

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   adminFuncUrl: 'http://utopia-alb-internet-facing-1935280894.us-east-2.elb.amazonaws.com/admin/',
-  authUrl:  'http://utopia-alb-internet-facing-1935280894.us-east-2.elb.amazonaws.com',
-  userUrl:  'http://utopia-alb-internet-facing-1935280894.us-east-2.elb.amazonaws.com',
-  adminUrl:  'http://utopia-alb-internet-facing-1935280894.us-east-2.elb.amazonaws.com',
+  authUrl:  'http://utopia-alb-internet-facing-1935280894.us-east-2.elb.amazonaws.com/',
+  userUrl:  'http://utopia-alb-internet-facing-1935280894.us-east-2.elb.amazonaws.com/',
+  adminUrl:  'http://utopia-alb-internet-facing-1935280894.us-east-2.elb.amazonaws.com/',
 };
