54f8bbd8bbb9d2a078d0c56fa32d11033f171e79
IanSmoothStack <ian.agostini@smoothstack.com>
2021-04-21 14:43:13 -0400

    Fixed things for merge

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,5 @@ export const environment = {
   adminFuncUrl: 'http://utopia-alb-internet-facing-1935280894.us-east-2.elb.amazonaws.com/admin/',
   authUrl:  'http://utopia-alb-internet-facing-1935280894.us-east-2.elb.amazonaws.com/',
   userUrl:  'http://utopia-alb-internet-facing-1935280894.us-east-2.elb.amazonaws.com/',
-<<<<<<< HEAD
-  adminUrl:  'http://utopia-alb-internet-facing-1935280894.us-east-2.elb.amazonaws.com/',
-=======
   adminUrl:  'http://utopia-alb-internet-facing-1935280894.us-east-2.elb.amazonaws.com',
->>>>>>> f8f01b180828fa0134da76e8dc19c0ddc65f4a5b
 };
